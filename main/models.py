b29794d0d2c7def1f0d173e14798431cf42428eb
Solers36 <mtestov@rambler.ru>
2021-03-05 14:58:15 +0300

    Создал табличку в базе данных. Добавил механизм вывода всех элементов базы данных, выделил страницу просмотра отдельной пести, с выводом данных из базы данных.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Accord(models.Model):
+    artist = models.CharField("Исполнитель", max_length=50, default="Неизвестный исполнитель")
+    song_title = models.CharField("Название произведения", max_length=150, default="Неизвестное название")
+    song_text = models.TextField("Аккорды и текст")
+    сhords_used =models.CharField("Используемые аккорды", max_length=250, default="Необязательное поле")
+    link_to_the_video = models.CharField("Ссылка на видео", max_length=250)
+
+    class Meta:
+        verbose_name = "Подбор аккордов"
+        verbose_name_plural = "Подборы аккордов"
+
+    def __str__(self):
+        return self.artist + ' - ' + self.song_title
--- main/views.py
+++ main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Accord
+from django.views.generic import DetailView
+
 
 def index(request):
-    return render(request, "main/index.html")
+    song = Accord.objects.all()
+    return render(request, "main/index.html", {'song': song, "title": "Accord"})
+
+class NewsDetailView(DetailView):
+    model = Accord
+    template_name = 'main/song.html'
+    context_object_name = 'song'
